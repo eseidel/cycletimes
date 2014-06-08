c94ca2678361069dd9dcc6d8ec4606cda8579e3e
Eric Seidel <eseidel@chromium.org>
2014-06-08 13:28:54 -0500

    Re-write commit_rate to be more like cycletimes in preparation for graphing

--- commit_rate.py
+++ commit_rate.py
@@ -9,12 +9,34 @@ import sys
 import os
 
 
+# FIXME: These could be shared with cycletimes.py
 ROOT = '/src/chromium/src'
 REPOSITORIES = [
-	'.',
-	'third_party/WebKit',
-	'third_party/skia/src',
-	'v8',
+    {
+        'name': 'chrome',
+        'relative_path': '.',
+    },
+    {
+        'name': 'blink',
+        'relative_path': 'third_party/WebKit',
+    },
+    {
+        'name': 'skia',
+        'relative_path': 'third_party/skia/src',
+    },
+    {
+        'name': 'v8',
+        'relative_path': 'v8',
+    },
+]
+
+FIELD_ORDER = [
+	'month',
+	'commits',
+	'contributors',
+	'mean_commits_per',
+	'median_commits_per',
+	'ninetieth_commits_per',
 ]
 
 
@@ -29,29 +51,38 @@ def _tuple_from_line(line):
 	return (date, author)
 
 
+def _tuples_for_respository(repository):
+	args = [
+		'git',
+		'log',
+		'--pretty=format:%ct###%an',
+	]
+	directory = os.path.join(ROOT, repository['relative_path'])
+	log_text = subprocess.check_output(args, cwd=directory)
+	return [_tuple_from_line(line) for line in log_text.split('\n')]
+
+
+def _stats_from_tuples(month, tuples):
+	stats = {}
+	counter = collections.Counter(map(lambda data_and_author: data_and_author[1], tuples))
+	stats['month'] = tuples[0][0].strftime('%m/%Y')
+	stats['commits'] = len(tuples)
+	stats['contributors'] = len(counter)
+	stats['mean_commits_per'] = round(numpy.mean(counter.values()), 1)
+	stats['median_commits_per'] = numpy.median(counter.values())
+	stats['ninetieth_commits_per'] = numpy.percentile(counter.values(), 90)
+	return stats
+
+
 def main(args):
 	for repository in REPOSITORIES:
-		directory = os.path.join(ROOT, repository)
 		print 
-		print directory
-		args = [
-			'git',
-			'log',
-			'--pretty=format:%ct###%an',
-		]
-		log_text = subprocess.check_output(args, cwd=directory)
-		tuples = [_tuple_from_line(line) for line in log_text.split('\n')]
+		print repository['name']
+		tuples = _tuples_for_respository(repository)
 		print ' '.join(['month   ', 'com', 'con', 'avg', 'med', '90%'])
 		for month, values in itertools.groupby(tuples, key=lambda date_and_author: date_and_author[0].month):
-			tuples_list = list(values)
-			counter = collections.Counter(map(lambda data_and_author: data_and_author[1], tuples_list))
-			month_string = tuples_list[0][0].strftime('%m/%Y')
-			commits = len(tuples_list)
-			contributors = len(counter)
-			mean_per = numpy.mean(counter.values())
-			median_per = numpy.median(counter.values())
-			ninty = numpy.percentile(counter.values(), 90)
-			print "%s %4d %3d %.1f %.1f %.1f" % (month_string, commits, contributors, mean_per, median_per, ninty)
+			stats = _stats_from_tuples(month, list(values))
+			print ' '.join(map(str, map(lambda name: stats[name], FIELD_ORDER)))
 
 
 if __name__ == "__main__":
