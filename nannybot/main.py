7328c797b4660d49fdca9446a48756d99e4a143f
Eric Seidel <eseidel@chromium.org>
2014-07-08 11:50:16 -0700

    Add the ability to create ignore records

--- nannybot/main.py
+++ nannybot/main.py
@@ -1,23 +1,51 @@
 import webapp2
-
 from google.appengine.ext import ndb
 import json
 import calendar
+import datetime
+
+
+class DateTimeEncoder(json.JSONEncoder):
+    def default(self, obj):
+        # FIXME: This should be UTC.
+        if isinstance(obj, datetime.datetime):
+            return calendar.timegm(obj.timetuple())
+        # Let the base class default method raise the TypeError
+        return json.JSONEncoder.default(self, obj)
+
 
 class AlertBlob(ndb.Model):
     date = ndb.DateTimeProperty(auto_now_add=True)
     content = ndb.StringProperty(indexed=False)
 
 
-class Data(webapp2.RequestHandler):
+class IgnoreRule(ndb.Model):
+    date = ndb.DateTimeProperty(auto_now_add=True)
+    pattern = ndb.StringProperty(indexed=False)
+
+
+class IgnoreHandler(webapp2.RequestHandler):
+    def get(self):
+        query = IgnoreRule.query().order(-IgnoreRule.date)
+        ignore_dicts = map(ndb.Model.to_dict, query.fetch())
+        self.response.write(json.dumps(ignore_dicts, cls=DateTimeEncoder))
+
+    def post(self):
+        ignore = IgnoreRule()
+        ignore.pattern = self.request.get('pattern')
+        ignore.put()
+        self.get()
+
+
+class DataHandler(webapp2.RequestHandler):
     def get(self):
         query = AlertBlob.query().order(-AlertBlob.date)
         entries = query.fetch(1)
         response_json = {
-            'date': calendar.timegm(entries[0].date.timetuple()) if entries else None,
+            'date': entries[0].date if entries else None,
             'content': json.loads(entries[0].content) if entries else None,
         }
-        self.response.write(json.dumps(response_json))
+        self.response.write(json.dumps(response_json, cls=DateTimeEncoder))
 
     def post(self):
         alert = AlertBlob()
@@ -26,5 +54,6 @@ class Data(webapp2.RequestHandler):
 
 
 app = webapp2.WSGIApplication([
-    ('/data', Data),
+    ('/data', DataHandler),
+    ('/ignore', IgnoreHandler),
 ], debug=True)
