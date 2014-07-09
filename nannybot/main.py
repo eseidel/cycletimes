dbff140665b704b03fea9e0b15564bf0477c348a
Eric Seidel <eseidel@chromium.org>
2014-07-08 17:28:01 -0700

    Add support for computing likely revisions.

--- nannybot/main.py
+++ nannybot/main.py
@@ -51,19 +51,75 @@ class IgnoreHandler(webapp2.RequestHandler):
         ignore.put()
         self.get()
 
+
+# Git ready, but only implemented for SVN atm.
+def ids_after_first_including_second(first, second):
+    return range(int(first) + 1, int(second) + 1)
+
+
+# Git ready, but only implemented for SVN atm.
+def is_ancestor_of(older, younger):
+    return int(older) < int(younger)
+
+
+def is_decendant_of(younger, older):
+    return is_ancestor_of(older, younger)
+
+
+def flatten_to_commit_list(passing, failing):
+    # Flatten two commit dicts to a list of 'name:commit'
+    if not passing or not failing:
+        return None
+    all_commits = []
+    for name in passing.keys():
+        commits = ids_after_first_including_second(passing[name], failing[name])
+        all_commits.extend(['%s:%s' % (name, commit) for commit in commits])
+    return all_commits
+
+
+def lookup_and_compare(existing, new, compare):
+    if not existing or compare(existing, new):
+        return new
+    return existing
+
+
+# FIXME: Perhaps this would be cleaner as:
+# passing = find_maximal(alert, 'passing_revisions', is_ancestor_of)
+# failing = find_maximal(alert, 'failing_revisions', is_decendant_of)
+def merge_regression_ranges(alerts):
+    last_passing = {}
+    first_failing = {}
+    for alert in alerts:
+        passing = alert['passing_revisions']
+        if not passing:
+            continue
+        failing = alert['failing_revisions']
+        for name in passing.keys():
+            last_passing[name] = lookup_and_compare(last_passing.get(name), passing.get(name), is_ancestor_of)
+            first_failing[name] = lookup_and_compare(first_failing.get(name), failing.get(name), is_decendant_of)
+    return last_passing, first_failing
+
+
+def reason_key_for_alert(alert):
+    # FIXME: May need something smarter for reason_key.
+    reason_key = alert['step_name']
+    if alert['piece']:
+        reason_key += ':%s' % alert['piece']
+    return reason_key
+
+
 def make_reason_groups(alerts):
     by_reason = collections.defaultdict(list)
     for alert in alerts:
-        # FIXME: Probably want something smarter here.
-        reason_key = alert['step_name']
-        if alert['piece']:
-            reason_key += ':%s' % alert['piece']
-        by_reason[reason_key].append(alert)
+        by_reason[reason_key_for_alert(alert)].append(alert)
 
     groups = []
     for reason_key, alerts in by_reason.items():
+        last_passing, first_failing = merge_regression_ranges(alerts)
+        blame_list = flatten_to_commit_list(last_passing, first_failing)
         groups.append({
             'reason_key': reason_key,
+            'likely_revisions': blame_list,
             # FIXME: These should probably be a list of keys
             # once alerts have keys.
             'failures': alerts,
