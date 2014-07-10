3c4979eb49ce15a73710d0e301568b625fb5e3c7
Eric Seidel <eseidel@chromium.org>
2014-07-09 17:25:05 -0700

    rewrite merge_regression_ranges to not return empty for first_failing when last_passing is empty.
    Also test flatten_to_commit_list and write a more
    sophisticated range_key function for a nicer grouping heuristic.

--- nannybot/analysis.py
+++ nannybot/analysis.py
@@ -27,6 +27,14 @@ def is_decendant_of(younger, older):
     return is_ancestor_of(older, younger)
 
 
+def commit_compare(one, two):
+    if is_decendant_of(one, two):
+        return -1
+    if is_ancestor_of(one, two):
+        return 1
+    return 0 # This is technically not right, since commits can be non-comparable.
+
+
 def flatten_to_commit_list(passing, failing):
     # Flatten two commit dicts to a list of 'name:commit'
     if not passing or not failing:
@@ -38,11 +46,6 @@ def flatten_to_commit_list(passing, failing):
     return all_commits
 
 
-def lookup_and_compare(existing, new, compare):
-    if not existing or compare(existing, new):
-        return new
-    return existing
-
 # FIXME: Perhaps this should be done by the feeder?
 def assign_keys(alerts):
     for key, alert in enumerate(alerts):
@@ -51,20 +54,27 @@ def assign_keys(alerts):
     return alerts
 
 
-# FIXME: Perhaps this would be cleaner as:
-# passing = find_maximal(alert, 'passing_revisions', is_ancestor_of)
-# failing = find_maximal(alert, 'failing_revisions', is_decendant_of)
 def merge_regression_ranges(alerts):
-    last_passing = {}
-    first_failing = {}
-    for alert in alerts:
-        passing = alert['passing_revisions']
-        if not passing:
-            continue
-        failing = alert['failing_revisions']
-        for name in passing.keys():
-            last_passing[name] = lookup_and_compare(last_passing.get(name), passing.get(name), is_ancestor_of)
-            first_failing[name] = lookup_and_compare(first_failing.get(name), failing.get(name), is_decendant_of)
+    def make_merge_dicts(reducer):
+        def merge_dicts(one, two):
+            if not one or not two:
+                return None
+            reduction = {}
+            for key in one.keys():
+                reduction[key] = reducer(one[key], two[key])
+            return reduction
+        return merge_dicts
+
+    # These don't handle the case where commits can't be compared.
+    older_commit = lambda one, two: one if is_ancestor_of(one, two) else two
+    younger_commit = lambda one, two: one if is_decendant_of(one, two) else two
+
+    passing_dicts = map(operator.itemgetter('passing_revisions'), alerts)
+    last_passing = reduce(make_merge_dicts(younger_commit), passing_dicts)
+
+    failing_dicts = map(operator.itemgetter('failing_revisions'), alerts)
+    first_failing = reduce(make_merge_dicts(older_commit), failing_dicts)
+
     return last_passing, first_failing
 
 
@@ -85,11 +95,12 @@ def group_by_reason(alerts):
     for reason_key, alerts in by_reason.items():
         last_passing, first_failing = merge_regression_ranges(alerts)
         blame_list = flatten_to_commit_list(last_passing, first_failing)
+        # FIXME: blame_list isn't filtered yet, but should be.
         reason_groups.append({
             'sort_key': reason_key,
+            'merged_last_passing': last_passing,
+            'merged_first_failing': first_failing,
             'likely_revisions': blame_list,
-            # FIXME: These should probably be a list of keys
-            # once alerts have keys.
             'failure_keys': map(operator.itemgetter('key'), alerts),
         })
     return reason_groups
@@ -109,19 +120,40 @@ def longestSubstringFinder(string1, string2):
     return answer
 
 
+def range_key_for_group(group):
+    last_passing = group['merged_last_passing']
+    first_failing = group['merged_first_failing']
+    if last_passing:
+        range_key = ' '.join(flatten_to_commit_list(last_passing, first_failing))
+    else:
+        # Even regressions where we don't know when they started can be
+        # merged by our earliest known failure.
+        parts = ['<=%s:%s' % (name, commit) for name, commit in first_failing.items()]
+        range_key = ' '.join(parts)
+    # sort_key is a heuristic to avoid merging failiures like
+    # gclient revert + webkit_tests which just happened to pull
+    # exact matching revisions when failing.
+    return range_key + group['sort_key'][:3]
+
+
 def merge_by_range(reason_groups):
+    expected_keys = sorted(reason_groups[0].keys())
     by_range = {}
     for group in reason_groups:
-        range_key = ' '.join(sorted(group['likely_revisions']))
+        range_key = range_key_for_group(group)
         existing = by_range.get(range_key)
         if not existing:
-            by_range[range_key] = group
+            # Shallow copy of group.
+            by_range[range_key] = dict(group)
             continue
 
-        by_range[range_key] = {
+        # FIXME: It's possible we don't want to merge two keys with nothing in common.
+        # e.g. bot_update and
+        # We only care about these two keys, the rest should be the same between all groups.
+        # I guess we could assert that...
+        by_range[range_key].update({
             'sort_key': longestSubstringFinder(existing['sort_key'], group['sort_key']),
-            'likely_revisions': existing['likely_revisions'],
             'failure_keys': sorted(set(existing['failure_keys'] + group['failure_keys'])),
-        }
+        })
 
     return sorted(by_range.values(), key=operator.itemgetter('sort_key'))
--- nannybot/analysis_unittest.py
+++ nannybot/analysis_unittest.py
@@ -8,48 +8,82 @@ import json
 
 
 class FailureAnalysisTest(unittest.TestCase):
+    # v8 stays the same, chromium differs, nacl is reduced.
     MERGE_REGRESSION_RANGES_JSON = """
 [
     {
      "failing_revisions": {
       "v8": "22263",
       "chromium": "282006",
-      "nacl": "13441",
-      "blink": "177644"
+      "nacl": "13441"
      },
      "passing_revisions": {
       "v8": "22263",
       "chromium": "281980",
-      "nacl": "13441",
-      "blink": "177644"
+      "nacl": "13441"
      }
     },
     {
      "failing_revisions": {
       "v8": "22263",
       "chromium": "282022",
-      "nacl": "13452",
-      "blink": "177644"
+      "nacl": "13452"
      },
      "passing_revisions": {
       "v8": "22263",
       "chromium": "281989",
-      "nacl": "13441",
-      "blink": "177644"
+      "nacl": "13441"
+     }
+    }
+]
+"""
+
+    MERGE_REGRESSION_RANGES_JSON_NULL = """
+[
+    {
+     "failing_revisions": {
+      "v8": "22263",
+      "nacl": "13441"
+     },
+     "passing_revisions": null
+    },
+    {
+     "failing_revisions": {
+      "v8": "22263",
+      "nacl": "13452"
+     },
+     "passing_revisions": {
+      "v8": "22263",
+      "nacl": "13441"
      }
     }
 ]
 """
 
     def test_merge_regression_ranges(self):
-        # This test could be much more complicated, right now
-        # chromium is the only revision which differs.
         alerts = json.loads(self.MERGE_REGRESSION_RANGES_JSON)
         passing, failing = analysis.merge_regression_ranges(alerts)
-        expected_pass = { 'v8': '22263', 'chromium': '281989', 'blink': '177644', 'nacl': '13441' }
-        expected_fail = { 'v8': '22263', 'chromium': '282006', 'blink': '177644', 'nacl': '13441' }
+        expected_pass = { 'v8': '22263', 'chromium': '281989', 'nacl': '13441' }
+        expected_fail = { 'v8': '22263', 'chromium': '282006', 'nacl': '13441' }
         self.assertEquals(expected_fail, failing)
         self.assertEquals(expected_pass, passing)
+
+        alerts = json.loads(self.MERGE_REGRESSION_RANGES_JSON_NULL)
+        passing, failing = analysis.merge_regression_ranges(alerts)
+        expected_pass = None
+        expected_fail = { 'v8': '22263', 'nacl': '13441' }
+        self.assertEquals(expected_fail, failing)
+        self.assertEquals(expected_pass, passing)
+
+
+    def test_flatten_to_commit_list(self):
+        passing = { 'v8': '1', 'chromium': '4'}
+        failing = { 'v8': '2', 'chromium': '4'}
+        commit_list = analysis.flatten_to_commit_list(passing, failing)
+        self.assertEquals(commit_list, ['v8:2'])
+
+        commit_list = analysis.flatten_to_commit_list(None, failing)
+        self.assertEquals(commit_list, ['<=v8:2', '<=chromium:4'])
 
     MERGE_BY_RANGE_JSON = """
 [
@@ -61,7 +95,7 @@ class FailureAnalysisTest(unittest.TestCase):
     "chromium:282022"
    ],
    "sort_key": "dromaeo.domcoreattr",
-   "failures": [
+   "failure_keys": [
    ]
   },
   {
@@ -72,7 +106,7 @@ class FailureAnalysisTest(unittest.TestCase):
     "chromium:281990"
    ],
    "sort_key": "dromaeo.jslibmodifyprototype",
-   "failures": [
+   "failure_keys": [
    ]
   }
 ]
