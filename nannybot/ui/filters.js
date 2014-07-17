d870df2ecd9990f2c8fe36bf5d368c31f848eb5c
Eric Seidel <eseidel@chromium.org>
2014-07-17 16:35:59 -0700

    Split out nannybot.html into components.

---
+++ nannybot/ui/filters.js
@@ -0,0 +1,38 @@
+// Needed to make filters visible inside sortable-table.
+  PolymerExpressions.prototype.since_string = function(value) {
+    // Buildbot talks to us in seconds, js expect milliseconds
+    date = new Date(value * 1000.0);
+    return humanized_time_span(date);
+  }
+  PolymerExpressions.prototype.falsy_to_empty = function(value) {
+    return value ? value : '';
+  }
+  PolymerExpressions.prototype.master_name = function(value) {
+    if (!value)
+      return 'foo';
+    var url_parts = value.split('/');
+    var long_name = url_parts[url_parts.length - 1];
+    if (long_name.indexOf('chromium.') == 0) {
+      return long_name.slice('chromium.'.length);
+    }
+    return long_name;
+  }
+  PolymerExpressions.prototype.builder_url = function(failure) {
+    return failure.master_url + '/builders/' + failure.builder_name;
+  }
+  PolymerExpressions.prototype.build_url = function(failure, build_number) {
+    return PolymerExpressions.prototype.builder_url(failure) + '/builds/' + build_number;
+  }
+  PolymerExpressions.prototype.step_url = function(failure, build_number) {
+    return PolymerExpressions.prototype.build_url(failure, build_number) + '/steps/' + failure.step_name;
+  }
+  PolymerExpressions.prototype.stdio_url = function(failure, build_number) {
+    return PolymerExpressions.prototype.step_url(failure, build_number) + '/logs/stdio';
+  }
+  PolymerExpressions.prototype.flakiness_dashboard_url = function(test_name, step_name) {
+    if (!test_name)
+      return '';
+    if (step_name.indexOf('test') == -1)
+      return '';
+    return "http://test-results.appspot.com/dashboards/flakiness_dashboard.html#testType=" + step_name + "&tests=" + encodeURIComponent(test_name);
+  }
