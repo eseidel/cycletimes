3b5a6b2ee7f7843f4351f30f39f89935bc5be06b
Eric Seidel <eseidel@chromium.org>
2014-07-17 17:57:31 -0700

    Only feed production server by default.

--- nannybot/feeder_start.sh
+++ nannybot/feeder_start.sh
@@ -7,6 +7,6 @@ do
     cd /src/cycletimes/nannybot
 	git pull
 
-	./feeder.py http://auto-sheriff.appspot.com/data http://localhost:8080/data
+	./feeder.py http://auto-sheriff.appspot.com/data
 	sleep 1 # Allow time for second control-C
 done
