6c7bc62fb695936811b7207b9940897231a88f53
Eric Seidel <eseidel@chromium.org>
2014-07-20 16:16:14 -0700

    Teach flakiness dashboard urls about groups

--- nannybot/ui/filters.js
+++ nannybot/ui/filters.js
@@ -29,10 +29,45 @@ PolymerExpressions.prototype.step_url = function(failure, build_number) {
 PolymerExpressions.prototype.stdio_url = function(failure, build_number) {
   return PolymerExpressions.prototype.step_url(failure, build_number) + '/logs/stdio';
 }
-PolymerExpressions.prototype.flakiness_dashboard_url = function(test_name, step_name) {
+
+// This map is matches the test-results code:
+// https://code.google.com/p/chromium/codesearch#chromium/src/third_party/WebKit/Tools/TestResultServer/handlers/buildershandler.py&l=40
+var MASTERS = [
+    {'name': 'ChromiumWin', 'url_name': 'chromium.win', 'groups': ['@ToT Chromium']},
+    {'name': 'ChromiumMac', 'url_name': 'chromium.mac', 'groups': ['@ToT Chromium']},
+    {'name': 'ChromiumLinux', 'url_name': 'chromium.linux', 'groups': ['@ToT Chromium']},
+    {'name': 'ChromiumChromiumOS', 'url_name': 'chromium.chromiumos', 'groups': ['@ToT ChromeOS']},
+    {'name': 'ChromiumGPU', 'url_name': 'chromium.gpu', 'groups': ['@ToT Chromium']},
+    {'name': 'ChromiumGPUFYI', 'url_name': 'chromium.gpu.fyi', 'groups': ['@ToT Chromium FYI']},
+    {'name': 'ChromiumWebkit', 'url_name': 'chromium.webkit', 'groups': ['@ToT Chromium', '@ToT Blink']},
+    {'name': 'ChromiumFYI', 'url_name': 'chromium.fyi', 'groups': ['@ToT Chromium FYI']},
+    {'name': 'GpuTryServer', 'url_name': 'tryserver.chromium.gpu', 'groups': ['TryServers']},
+    {'name': 'V8', 'url_name': 'client.v8', 'groups': ['@ToT V8']},
+];
+
+function masterNameFromURL(master_url) {
+  var parts = master_url.trimRight('/').split('/')
+  return parts[parts.length - 1]
+}
+
+// FIXME: test-results supports multiple 'groups' but it's an unused feature.
+// It's not clear that any given waterfall should ever be in more than one 'tree'.
+function groupForMaster(master_url) {
+  var name = masterNameFromURL(master_url);
+  for (var x = 0; x < MASTERS.length; ++x) {
+    var record = MASTERS[x];
+    if (record['url_name'] == name)
+      return record['groups'][0];
+  }
+}
+
+PolymerExpressions.prototype.flakiness_dashboard_url = function(test_name, step_name, master_url) {
   if (!test_name)
     return '';
   if (step_name.indexOf('test') == -1)
     return '';
-  return "http://test-results.appspot.com/dashboards/flakiness_dashboard.html#testType=" + step_name + "&tests=" + encodeURIComponent(test_name);
+  return "http://test-results.appspot.com/dashboards/flakiness_dashboard.html#"
+    + "testType=" + step_name
+    + "&tests=" + encodeURIComponent(test_name)
+    + "&group=" + groupForMaster(master_url);
 }
