225054c5c1f5647710e4cf11daf5ade7cc373a74
Eric Seidel <eseidel@chromium.org>
2014-07-23 11:11:40 -0700

    List tree closers by tree instead of master.

--- nannybot/tree_closers.py
+++ nannybot/tree_closers.py
@@ -2,10 +2,14 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import sys
+import base64
 import buildbot
+import collections
 import gatekeeper_extras
+import json
 import os
+import requests
+import sys
 
 # This is relative to build/scripts:
 # https://chromium.googlesource.com/chromium/tools/build/+/master/scripts
@@ -18,17 +22,29 @@ from slave import gatekeeper_ng_config
 # https://chromium.googlesource.com/chromium/tools/build/+/master/scripts/slave/gatekeeper.json?format=TEXT
 CONFIG_PATH = os.path.join(BUILD_SCRIPTS_PATH, 'slave', 'gatekeeper.json')
 
+TREES_JSON = "https://chromium.googlesource.com/chromium/tools/build/+/master/scripts/slave/gatekeeper_trees.json?format=TEXT"
 
 def main(args):
     gatekeeper = gatekeeper_ng_config.load_gatekeeper_config(CONFIG_PATH)
 
-    tree_closers = {}
-    for master_url, master_config in gatekeeper.items():
-        master_name = buildbot.master_name_from_url(master_url)
-        master_json = buildbot.fetch_master_json(master_url)
-        builder_names = set(master_json['builders'].keys())
-        builder_names -= gatekeeper_extras.excluded_builders(master_config)
-        tree_closers[master_name] = builder_names
+    # WTF?  Why is Gitles format=text base64 encoding??
+    trees_encoded = requests.get(TREES_JSON).text
+    trees = json.loads(base64.b64decode(trees_encoded))
+
+    tree_closers = collections.defaultdict(list)
+    for tree_name, tree_config in trees.items():
+        for master_url in tree_config['masters']:
+            master_config = gatekeeper[master_url]
+            if not master_config:
+                print "ERROR: gatekeeper missing %s" % master_url
+                continue
+
+            master_name = buildbot.master_name_from_url(master_url)
+            master_json = buildbot.fetch_master_json(master_url)
+            builder_names = set(master_json['builders'].keys())
+            builder_names -= gatekeeper_extras.excluded_builders(master_config)
+            master_names = [master_name] * len(builder_names)
+            tree_closers[tree_name].extend(zip(master_names, builder_names))
 
     # FIXME: This is not fully accurate.  It's possible for a non-excluded
     # builder to never be able to close the tree because none of its
@@ -37,11 +53,11 @@ def main(args):
     # and taking a representive sample of step names (probably from last passing build).
     # Then we could use gatekeeper_extras.would_close_tree on each step name.
 
-    for master_name in sorted(tree_closers.keys()):
-        print master_name
-        builder_names = sorted(tree_closers[master_name])
-        for builder_name in builder_names:
-            print '  ', builder_name
+    for tree_name in sorted(tree_closers.keys()):
+        print tree_name
+        builder_tuples = sorted(tree_closers[tree_name])
+        for master_name, builder_name in builder_tuples:
+            print '  %s: %s' % (master_name, builder_name)
 
 
 if __name__ == '__main__':
